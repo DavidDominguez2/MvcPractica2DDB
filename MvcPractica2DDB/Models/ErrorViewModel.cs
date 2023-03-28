8447efb0c993c2b5be7a343c8a8e8d44688c1220
David Domínguez Blanco <david.dominguez.blanco.2001@gmail.com>
2023-03-28 09:18:14 +0200

    Add project files.

---
+++ MvcPractica2DDB/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace MvcPractica2DDB.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
